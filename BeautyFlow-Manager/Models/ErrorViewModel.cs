6c584bb20ed18695caf0ecd757a81c17df090042
Raúl Armando Pérez Roig <110783683+Raulp1234@users.noreply.github.com>
2026-04-19 17:08:34 -0400

    inicial

---
+++ BeautyFlow-Manager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BeautyFlow_Manager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
