5bab59b84f8b712dcfe458b0878809ce3fcf3e6a
Raúl Armando Pérez Roig <110783683+Raulp1234@users.noreply.github.com>
2026-04-23 00:56:47 -0400

    1

--- BeautyFlow-Manager/obj/Debug/net8.0/BeautyFlow-Manager.AssemblyInfo.cs
+++ BeautyFlow-Manager/obj/Debug/net8.0/BeautyFlow-Manager.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BeautyFlow-Manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63e1df4a0993dffa88ccd2d4ba537f8bab9235f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c680b516d931d4ba9e823a5666eeaaa484c53c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BeautyFlow-Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BeautyFlow-Manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
